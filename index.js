28c198a229be5a0f2b7a7b665245b02f8b8db5c7
marina <marinaaltesmagret@gmail.com>
2021-04-24 18:21:47 +0200

    commit inicial

---
+++ index.js
@@ -0,0 +1,10 @@
+/* protagonistes: 2-10,
+locs, 
+acció.
+
+Poder eliminar o afegir categories.
+
+Full de justificant del sorteig: avui, amb data tal, hora tal, hem fet un sorteig amb els resultats tal.
+
+
+fase 1 codi (30/04)
